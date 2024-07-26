0b080277e0c117d15267f5d111770d091cbc37a7
mring33621 <matthew_s_ring@yahoo.com>
2024-07-25 22:08:16 -0500

    adjusted supported models list

--- src/test/java/xyz/mattring/grambaal/oai/GPTModelHelperTest.java
+++ src/test/java/xyz/mattring/grambaal/oai/GPTModelHelperTest.java
@@ -16,14 +16,16 @@ class GPTModelHelperTest {
 //        System.out.println(observed);
         String expected =
                 "nvidia/Nemotron-4-340B-Instruct, 4000, 202306\n" +
-                        "meta-llama/Meta-Llama-3-70B-Instruct, 8000, 202303\n" +
                         "gpt-3.5-turbo, 16385, 202109\n" +
                         "gemini-1.0-pro-latest, 30720, 202303\n" +
                         "Qwen/Qwen2-72B-Instruct, 32000, 202307\n" +
+                        "meta-llama/Meta-Llama-3.1-405B-Instruct, 32000, 202303\n" +
                         "gpt-4-turbo, 128000, 202312\n" +
                         "gpt-4o, 128000, 202310\n" +
+                        "gpt-4o-mini, 128000, 202310\n" +
+                        "meta-llama/Meta-Llama-3.1-70B-Instruct, 128000, 202303\n" +
                         "claude-3-haiku-20240307, 200000, 202308\n" +
-                        "claude-3-sonnet-20240229, 200000, 202308\n" +
+                        "claude-3-opus-20240229, 200000, 202308\n" +
                         "claude-3-5-sonnet-20240620, 200000, 202404\n" +
                         "gemini-1.5-pro-latest, 1048576, 202303\n" +
                         "gemini-1.5-flash-latest, 1048576, 202303";
@@ -42,6 +44,6 @@ class GPTModelHelperTest {
 
     @org.junit.jupiter.api.Test
     void getModelForEnumName() {
-        assertEquals(GPTModel.DINFRA_LLAMA_3_70B, GPT_MODEL_HELPER.getModelForEnumName("DINFRA_LLAMA_3_70B").get());
+        assertEquals(GPTModel.DINFRA_LLAMA_3_1_70B, GPT_MODEL_HELPER.getModelForEnumName("DINFRA_LLAMA_3_1_70B").get());
     }
 }
