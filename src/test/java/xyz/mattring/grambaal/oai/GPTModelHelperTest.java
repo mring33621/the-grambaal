78999910bb55819497e36295f2bba5535938b13d
mring33621 <matthew_s_ring@yahoo.com>
2025-02-05 20:49:26 -0600

    Removed some old Llamas and added some Gemini

--- src/test/java/xyz/mattring/grambaal/oai/GPTModelHelperTest.java
+++ src/test/java/xyz/mattring/grambaal/oai/GPTModelHelperTest.java
@@ -19,15 +19,12 @@ class GPTModelHelperTest {
                         "deepseek-ai/DeepSeek-R1, 16000, 202401\n" +
                         "Qwen/QwQ-32B-Preview, 32000, 202401\n" +
                         "Qwen/Qwen2.5-Coder-32B-Instruct, 32000, 202401\n" +
-                        "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo, 32000, 202401\n" +
-                        "meta-llama/Llama-3.3-70B-Instruct-Turbo, 32000, 202401\n" +
-                        "meta-llama/Meta-Llama-3.1-405B-Instruct, 32000, 202401\n" +
                         "mistral-small-latest, 32000, 202404\n" +
                         "NovaSky-AI/Sky-T1-32B-Preview, 32768, 202401\n" +
-                        "chatgpt-4o-latest, 128000, 202310\n" +
-                        "o1-preview, 128000, 202310\n" +
+                        "gpt-4o-mini, 128000, 202310\n" +
                         "o1-mini, 128000, 202310\n" +
                         "nvidia/Llama-3.1-Nemotron-70B-Instruct, 128000, 202401\n" +
+                        "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B, 128000, 202401\n" +
                         "ministral-8b-latest, 128000, 202404\n" +
                         "deepseek-ai/DeepSeek-R1-Distill-Llama-70B, 131000, 202401\n" +
                         "mistral-large-latest, 131000, 202404\n" +
@@ -35,22 +32,22 @@ class GPTModelHelperTest {
                         "claude-3-5-haiku-20241022, 200000, 202407\n" +
                         "claude-3-5-sonnet-20241022, 200000, 202404\n" +
                         "codestral-latest, 256000, 202404\n" +
-                        "gemini-1.5-pro, 1048576, 202409\n" +
-                        "gemini-1.5-flash, 1048576, 202409\n" +
-                        "gemini-2.0-flash-exp, 1048576, 202411\n" +
+                        "gemini-2.0-flash-lite-preview-02-05, 1048576, 202411\n" +
+                        "gemini-2.0-flash-001, 1048576, 202411\n" +
                         "gemini-2.0-flash-thinking-exp-01-21, 1048576, 202411\n" +
-                        "gemini-exp-1206, 1048576, 202411";
+                        "gemini-exp-1206, 1048576, 202411\n" +
+                        "gemini-2.0-pro-exp-02-05, 1048576, 202411";
         assertEquals(expected, observed);
     }
 
     @org.junit.jupiter.api.Test
     void getModelForTokens() {
-        assertEquals(GPTModel.GPT_4o, GPT_MODEL_HELPER.getModelForMaxTokens(128000).get());
+        assertEquals(GPTModel.GPT_4o_mini, GPT_MODEL_HELPER.getModelForMaxTokens(128000).get());
     }
 
     @org.junit.jupiter.api.Test
     void getModelForModelName() {
-        assertEquals(GPTModel.GEM_1_5_FLASH_LATEST, GPT_MODEL_HELPER.getModelForModelName("gemini-1.5-flash").get());
+        assertEquals(GPTModel.DINFRA_DEEPSEEK_V3, GPT_MODEL_HELPER.getModelForModelName("deepseek-ai/DeepSeek-V3").get());
     }
 
     @org.junit.jupiter.api.Test
